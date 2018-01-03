84dbbd9f3f11650422c37623df73328e07088e87
Lucas Arundell <larundell@small-improvements.com>
2018-01-03 16:46:11 +0100

    feat(ExampleApp) add basic example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import withAnimation from 'react-with-animation';
 
+function Example(props) {
+    return <div {...props}>EXAMPLE</div>;
+}
+const ExampleWithAnimation = withAnimation(Example);
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ExampleWithAnimation
+                    animateOnFirstRender={true}
+                    animationClasses={'Example-animating'}
+                    animationDuration={2000}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
