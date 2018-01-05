01862a73fdf9ee48c33d9c7b86b8e365cc4fc9b8
Lucas Arundell <larundell@small-improvements.com>
2018-01-05 16:04:48 +0100

    feat(App) update docs

--- src/components/ExampleContent/content.js
+++ src/components/ExampleContent/content.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import withAnimation from 'react-with-animation';
 
-export const BasicExampleContent = props => <div {...props}>Sample Content</div>;
+export const BasicExampleContent = ({ className, style }) => (
+    <div className={className} style={style}>
+        Sample Content
+    </div>
+);
+
 export const ExampleAutoPlay = withAnimation(BasicExampleContent);
--- src/components/ExampleContent/index.js
+++ src/components/ExampleContent/index.js
@@ -7,27 +7,34 @@ import Section from './section';
 export default function ExampleContent() {
     return (
         <div className="ExampleContent">
-            <p>Hint: open the console and watch as the CSS classes are added/removed at the end of the animation</p>
+            <div>
+                <p>
+                    <a href="https://github.com/SmallImprovements/react-with-animation">
+                        https://github.com/SmallImprovements/react-with-animation
+                    </a>
+                </p>
+                <p>Hint: open the console and watch as the CSS classes are added/removed at the end of the animation</p>
+            </div>
             <Section>
-                <h2>Animate once when component renders</h2>
+                <h2>Animation manually</h2>
+                <AnimationPlayer
+                    className="ExampleContent-element"
+                    animationClasses="ExampleContent-animated"
+                    animateOnFirstRender={false}
+                    animationDuration={1000}
+                />
+                <small>Use the element.startAnimation() method</small>
+            </Section>
+            <Section>
+                <h2>Animate on render</h2>
                 <ExampleAutoPlay
                     className="ExampleContent-element"
                     animationClasses="ExampleContent-animated"
                     animateOnFirstRender={true}
                 />
-                <p>
+                <small>
                     <strong>Note:</strong> This animation will only play once (refresh the page if you missed it!)
-                </p>
-            </Section>
-            <Section>
-                <h2>Trigger the animation manually</h2>
-                <AnimationPlayer
-                    className="ExampleContent-element"
-                    animationClasses="ExampleContent-animated"
-                    animateOnFirstRender={true}
-                    animationDuration={1000}
-                />
-                <p>Use the element.startAnimation() method</p>
+                </small>
             </Section>
             <Section>
                 <h2>Coming soon: set animation-iteration-count</h2>
